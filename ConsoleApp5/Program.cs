9ca0f321941111e48d0f83994815676fc9ae5be5
Serj Tigranyan <serj-tigranyan@mail.ru>
2021-11-18 14:19:08 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace restless
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal a, b;
+            Console.Write("Стипендия студента за месяц: ");
+            a = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Расходы студента в первый месяц: ");
+            b = Convert.ToDecimal(Console.ReadLine());
+            for (int i = 0; i < 10; i++)
+            {
+                a += a;
+                b += b * 1.03m;
+            }
+            Console.WriteLine($"{b - a:F2} рублей;"); 
+            Console.ReadKey();
+        }
+    }
+}
